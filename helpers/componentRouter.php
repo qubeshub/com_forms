c9b902780e0916915256f0abafefe7505129ae44
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2018-12-04 20:34:31 +0000

    Implement ComponentRouter

---
+++ helpers/componentRouter.php
@@ -0,0 +1,114 @@
+<?php
+/*
+ * HUBzero CMS
+ *
+ * Copyright 2005-2015 HUBzero Foundation, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * HUBzero is a registered trademark of Purdue University.
+ *
+ * @package   hubzero-cms
+ * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Components\Forms\Helpers;
+
+$componentPath = Component::path('com_forms');
+
+use Hubzero\Utility\Arr;
+
+class ComponentRouter
+{
+
+	/**
+	 * Constructs ComponentRouter instance
+	 *
+	 * @param    array   $args   Instantiation state
+	 * @return   void
+	 */
+	public function __construct($args = [])
+	{
+		$this->_baseUrl = '/forms';
+	}
+
+	/**
+	 * Generates forms create URL
+	 *
+	 * @return   string
+	 */
+	public function formsCreateUrl()
+	{
+		$segments = ['forms', 'create'];
+
+		$url = $this->_generateUrl($segments);
+
+		return $url;
+	}
+
+	/**
+	 * Generates forms edit URL
+	 *
+	 * @param    int      $formId   ID of form to edit
+	 * @return   string
+	 */
+	public function formsEditUrl($formId)
+	{
+		$segments = ['forms', 'edit', $formId];
+
+		$url = $this->_generateUrl($segments);
+
+		return $url;
+	}
+
+	/**
+	 * Generates forms edit URL
+	 *
+	 * @param    int      $formId   ID of form to edit
+	 * @return   string
+	 */
+	public function formsUpdateUrl($formId)
+	{
+		$segments = ['forms', 'update', $formId];
+
+		$url = $this->_generateUrl($segments);
+
+		return $url;
+	}
+
+	/**
+	 * Generates URL based on given segments
+	 *
+	 * @param    array    $segments   URL segments
+	 * @return   string
+	 */
+	protected function _generateUrl($segments)
+	{
+		$url = $this->_baseUrl;
+
+		foreach ($segments as $segment)
+		{
+			$url .= "/$segment";
+		}
+
+		return $url;
+	}
+
+}
